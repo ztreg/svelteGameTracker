58b9336ac7f9a3938e2632892e8f88e8c6de321c
Jonas_Gertz <Jonas.Gertz@enfogroup.com>
2022-11-30 23:39:55 +0100

    yep

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,14 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			edge: false,
-			split: false
+			// if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
 		})
 	}
 };
